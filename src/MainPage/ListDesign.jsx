852b4ca6eff9cd51785d3dc4f163dec7ef3b13d1
EmotoyBunny <sonka.zaichkina@mail.ru>
2021-04-07 21:01:57 +0300

    fix2

---
+++ src/MainPage/ListDesign.jsx
@@ -0,0 +1,96 @@
+import React, {Component} from "react";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import {Link} from "react-router-dom";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import PersonIcon from "@material-ui/icons/Person";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+class  ListDesign extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalOpened: false
+        };
+    }
+
+    handleClickOpen = () => {
+        this.setState({modalOpened: true});
+    };
+
+    handleClose = () => {
+        this.setState({modalOpened: false});
+    };
+
+
+    render() {
+        return (
+            <div>
+                <Divider variant="inset" />
+                <div>
+                    <ListItem alignItems="flex-start">
+                        <Divider variant="inset" />
+                        <ListItemAvatar>
+                            <Avatar variant='rounded' src={this.props.img}/>
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={this.props.name}
+                            secondary={
+                                <React.Fragment>
+                                    {this.props.game}
+                                </React.Fragment>
+                            }
+                        />
+                        <Link to={"/" + this.props.id}>
+                            <Tooltip color="primary" title="Узнать больше">
+                                <IconButton aria-label="to learn more">
+                                    <PersonIcon/>
+                                </IconButton>
+                            </Tooltip>
+                        </Link>
+                        <Tooltip color="primary" title="Удалить">
+                            <IconButton onClick={this.handleClickOpen} aria-label="delete">
+                                <DeleteIcon/>
+                            </IconButton>
+                        </Tooltip>
+                    </ListItem>
+                </div>
+                <Divider variant="inset"/>
+                <Dialog
+                    open={this.state.modalOpened}
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{"Вы уверены в этом?"}</DialogTitle>
+                    <DialogActions>
+                        <Tooltip color="primary" title="Удалить">
+                            <IconButton onClick={this.handleClose} aria-label="delete">
+                                <CloseIcon/>
+                            </IconButton>
+                        </Tooltip>
+                        <Tooltip color="primary" title="Удалить">
+                            <IconButton onClick={() => this.props.getData()} aria-label="delete">
+                                <DoneAllIcon/>
+                            </IconButton>
+                        </Tooltip>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        )
+    }
+}
+
+
+export default ListDesign;
+
